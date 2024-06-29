9a8be152430e5f728947585555e08b9ed26a9e04
idil <idil.iman1999@gmail.com>
2024-06-30 01:57:45 +0800

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,21 @@ export const metadata: Metadata = {
     description: 'Calories Tracker AI',
     images: [
       {
-        url: 'https://calories-tracker-ai.vercel.app/og.png',
+        url: '/og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Calories Tracker AI',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://calories-tracker-ai.vercel.app/',
+    title: 'Calories Tracker AI',
+    description: 'Calories Tracker AI',
+    images: [
+      {
+        url: '/og.png',
         width: 1200,
         height: 630,
         alt: 'Calories Tracker AI',
